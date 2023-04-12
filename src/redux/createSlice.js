90edee904e2a0eacfe43a1b41c275d7c43ca5321
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-04-12 17:13:36 +0300

    summ

---
+++ src/redux/createSlice.js
@@ -0,0 +1,24 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const todoListSlice = createSlice({
+//   name: 'todoList',
+//   initialState: {
+//     todos: [],
+//     filterStr: 'All',
+//     filteredTodos: [],
+//   },
+//   reducers: {
+//     handleAddTodo(title) {
+//       setTodos([
+//         ...todos,
+//         { id: new Date().getTime(), title, completed: false },
+//       ]);
+//     },
+
+//     handleDelete(state, action) {
+//       state.todos.filter(todo => todo.id !== action.payload);
+//     },
+//   },
+// });
+
+// export const {} = todoListSlice.actions;
